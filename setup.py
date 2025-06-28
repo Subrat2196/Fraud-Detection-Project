fbaf4e2d8be043d5e69dc2eb6aa99279f9d3c3e0
subrat bahuguna <bahuguna.subrat211996@gmail.com>
2025-06-28 14:32:22 +0530

    Created the directory structure using Cookie Cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Isme apun sare frauds ko pakad lega , koi bhi nahi chorega',
+    author='Subrat Bahuguna',
+    license='MIT',
+)
